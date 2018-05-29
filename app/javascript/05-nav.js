9176ca5016644daae1924b0b538db3143a7db919
Guillaume Poidevin <poidevin.guillaume@gmail.com>
2018-05-29 16:03:13 +0200

    slider

--- app/javascript/05-nav.js
+++ app/javascript/05-nav.js
@@ -1,7 +1,7 @@
 $(function () {
-    $('.icon-dehaze').click(function () {
+
+    $('body>header .icon-dehaze').click(function () {
         $('nav').slideToggle(1000);
     });
-
 
 }); // ne pas supprimer svp
