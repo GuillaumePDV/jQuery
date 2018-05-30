4ee9e627e3e212b9796cab4a4be3a3dccf0f6436
Guillaume Poidevin <poidevin.guillaume@gmail.com>
2018-05-30 12:12:53 +0200

    accordeon

---
+++ app/javascript/08-accordeon.js
@@ -0,0 +1,41 @@
+/**
+ * OUVRIR UN ITEM ET FERMER LES AUTRES
+ * Etape 1 : quand on clique sur un h1, si celui ci n'a pas la class active, 
+ * on ferme tous les p et on ouvre le p suivant le h1 cliqué
+ * - si il a la class active, c'est qu'il est deja ouvert donc on fait rien
+ 
+ * Etape 2 : on retire la class active de tous les h1 et on ajoute la class active sur 
+ * le h1 cliqué
+ * 
+ * Etape 3 : on retire l'icon-down sur tous les h1 et on ajouter icon-right sur ts les h1
+ * 
+ * Etape 4 : on alterne les class icon-down et icon-right sur this h1 (celui cliqué) 
+ */
+
+'use strict';
+
+$(function () {
+    $('.accordeon h1').click(function () {
+        if (!$(this).hasClass('active')) {
+            $('.accordeon p').slideUp(200);
+            $(this).next('p').slideDown();
+
+            $('.accordeon h1').removeClass('active');
+            $(this).addClass('active');
+
+            $('.accordeon h1 span')
+                .removeClass('icon-arrow_drop_down')
+                .addClass('icon-navigate_next');
+
+            // $(this).find('span')
+            //     .removeClass('icon-navigate_next')
+            //     .addClass('icon-arrow_drop_down');
+            $(this).find('span')
+                .toggleClass('icon-navigate_next icon-arrow_drop_down');
+
+        }
+    });
+
+
+
+}); // ne pas supprimer svp
--- gulpfile.js
+++ gulpfile.js
@@ -28,9 +28,10 @@ gulp.task('concat', function() {
             // './app/javascript/02-fondamenteaux.js',
             // './app/javascript/03-getters-setters.js',
             // './app/javascript/04-add-elem-dom.js',
-            // './app/javascript/05-nav.js',
-            // './app/javascript/06-slider.js',
+            './app/javascript/05-nav.js',
+            './app/javascript/06-slider.js',
             './app/javascript/07-lightbox.js',
+            './app/javascript/08-accordeon.js',
             
         ])
         .pipe(concat('production.js'))
